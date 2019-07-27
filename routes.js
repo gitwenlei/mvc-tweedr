c0664e3e939c8cb2f876c84d2655f711537796fd
gitwenlei <ngwenlei@gmail.com>
2019-07-27 20:03:23 +0800

    Added get registration and post new user request

--- routes.js
+++ routes.js
@@ -17,11 +17,23 @@ module.exports = (app, allModels) => {
     // ========================
     // GET home page
     // ========================
-    app.get('/tweedr', tweedrControllerCallbacks.index); // see tweedr_ctrl.js line 40
+    app.get('/tweedr', tweedrControllerCallbacks.index); // see tweedr_ctrl.js line 55
 
     // ========================
     // GET login page
     // ========================
-    app.get('/tweedr/login',tweedrControllerCallbacks.login); // see tweedr_ctrl.js line 41
+    app.get('/tweedr/login', tweedrControllerCallbacks.login); // see tweedr_ctrl.js line 56
+
+    // ========================
+    // GET register page
+    // ========================
+    app.get('/tweedr/register', tweedrControllerCallbacks.register); // see tweedr_ctrl.js line 57
+
+    // ========================
+    // POST users to register user
+    // ========================
+    app.post('/users', tweedrControllerCallbacks.users);  // see tweedr_ctrl.js
+
+
 
 };
