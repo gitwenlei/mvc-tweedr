dfc59d44dc6c1f5d753f2a1bcd6413e0d0d9833b
gitwenlei <ngwenlei@gmail.com>
2019-07-27 22:48:25 +0800

    Added login check

--- controllers/tweedr_ctrl.js
+++ controllers/tweedr_ctrl.js
@@ -6,28 +6,44 @@ module.exports = (db) => {
    * ===========================================
    */
 
-  let indexControllerCallback = (request, response) => { // function declared in routes.js
-      db.tweedr.getAll((error, allTweets) => {
-      // see db.js line 97 tweedr: tweedrModelsObject
-      // model file tweedr_model.js line 9
-      // getAll function - show all tweets
-        console.log(allTweets);
+    let indexControllerCallback = (request, response) => { // function declared in routes.js
+        db.tweedr.getAll((error, allTweets) => {
+        // see db.js line 97 tweedr: tweedrModelsObject
+        // model file tweedr_model.js line 9
+        // getAll function - show all tweets
+        // console.log(allTweets);
         response.render('tweedr/index', { allTweets }); //display index html page in views folder
-      });
-  };
+        });
+    };
 
     // ========================
     // Show login page
     // ========================
     let loginControllerCallback = (request, response) => { // function declared in routes.js
       db.tweedr.getLogin((error, allUsers) => {
-      // see db.js line 97 tweedr: tweedrModelsObject
-      // model file tweedr_model.js line 30
-      // getLogin function - show login pg
-        console.log(allUsers);
-        response.render('tweedr/login', { allUsers }); //display login html page in views folder
+        response.render('tweedr/login', { allUsers }); //display login form
       });
   };
+
+
+    // =======================================
+    // Check user login details
+    // ========================================
+    let checkControllerCallback = (request, response) => { // function declared in routes.js
+        db.tweedr.checkUsers(request.body, (error, results) => {
+            if (results !== null) {
+                // console.log(results);
+                let user_id = results[0].id;
+                response.cookie('loggedin', true);
+                response.cookie('user_id', user_id);
+                response.redirect('/tweedr');
+            } else {
+                console.log('invalid' , results);
+                console.log("incorrect password");
+                response.send('invalid input');
+            }
+        });
+    };
 
 
     // ========================
@@ -35,11 +51,8 @@ module.exports = (db) => {
     // ========================
     let registerControllerCallback = (request, response) => { // function declared in routes.js
       db.tweedr.getRegister((error, allUsers) => {
-      // see db.js line 97 tweedr: tweedrModelsObject
-      // model file tweedr_model.js line 52
-      // getRegisterfunction - show register pg
         console.log(allUsers);
-        response.render('tweedr/register', { allUsers }); //display register page in views folder
+        response.render('tweedr/register', { allUsers }); //display registration form
       });
     };
 
@@ -49,12 +62,8 @@ module.exports = (db) => {
     // ========================================
     let usersControllerCallback = (request, response) => { // function declared in routes.js
         db.tweedr.postUsers(request.body, (error, allUsers) => {
-        // see db.js line 97 tweedr: tweedrModelsObject
-        // see tweedr_model.js line 73
-        // postUsers function
-        // console.log("all users: " , allUsers)
-        response.send("Registration Successful!");
-        // response.render('tweedr/register', { allUsers }); //display register page in views folder
+        // response.send("Registration Successful!");
+        response.redirect('/tweedr');
         });
     };
 
@@ -72,5 +81,6 @@ module.exports = (db) => {
     login: loginControllerCallback, // see above line 22
     register: registerControllerCallback, // see above line 36
     users: usersControllerCallback, // see above line 50
+    check: checkControllerCallback,
   };
 }
