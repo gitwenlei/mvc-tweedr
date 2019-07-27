4ee64afadab35c560a3456f3680141112efeaa49
gitwenlei <ngwenlei@gmail.com>
2019-07-27 20:01:13 +0800

    Added user registration form request & add new user to database controller

--- controllers/tweedr_ctrl.js
+++ controllers/tweedr_ctrl.js
@@ -30,6 +30,37 @@ module.exports = (db) => {
   };
 
 
+    // ========================
+    // Show register page
+    // ========================
+    let registerControllerCallback = (request, response) => { // function declared in routes.js
+      db.tweedr.getRegister((error, allUsers) => {
+      // see db.js line 97 tweedr: tweedrModelsObject
+      // model file tweedr_model.js line 52
+      // getRegisterfunction - show register pg
+        console.log(allUsers);
+        response.render('tweedr/register', { allUsers }); //display register page in views folder
+      });
+    };
+
+
+    // =======================================
+    // Show user registration successful page
+    // ========================================
+    let usersControllerCallback = (request, response) => { // function declared in routes.js
+        db.tweedr.postUsers(request.body, (error, allUsers) => {
+        // see db.js line 97 tweedr: tweedrModelsObject
+        // see tweedr_model.js line 73
+        // postUsers function
+        // console.log("all users: " , allUsers)
+        response.send("Registration Successful!");
+        // response.render('tweedr/register', { allUsers }); //display register page in views folder
+        });
+    };
+
+
+
+
   /**
    * ===========================================
    * Export controller functions as a module
@@ -39,5 +70,7 @@ module.exports = (db) => {
   return {
     index: indexControllerCallback, // see above line 9
     login: loginControllerCallback, // see above line 22
+    register: registerControllerCallback, // see above line 36
+    users: usersControllerCallback, // see above line 50
   };
 }
