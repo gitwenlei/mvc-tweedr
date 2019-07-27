7836eb54c63cc12671fbc7ed1228e9c077a60c5d
gitwenlei <ngwenlei@gmail.com>
2019-07-27 18:12:01 +0800

    Create login page

---
+++ views/tweedr/login.jsx
@@ -0,0 +1,29 @@
+var React = require('react');
+
+class Login extends React.Component {
+  render() {
+    return (
+      <html>
+        <body>
+          <div>
+            <h1>Welcome to Tweedr!</h1>
+            <h3>Please login</h3>
+            <form action="/login" method="POST">
+                <p>
+                    name
+                    <input name="name"/>
+                </p>
+                <p>
+                    password
+                    <input name="password"/>
+                </p>
+                <input type="submit"/>
+            </form>
+          </div>
+        </body>
+      </html>
+    );
+  }
+}
+
+module.exports = Login;
