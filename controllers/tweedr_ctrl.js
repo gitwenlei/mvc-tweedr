d89123fb02f5ea64680b6f6009ff1caa6a3c3a5a
gitwenlei <ngwenlei@gmail.com>
2019-07-27 15:45:23 +0800

    Added login page controller

--- controllers/tweedr_ctrl.js
+++ controllers/tweedr_ctrl.js
@@ -7,9 +7,25 @@ module.exports = (db) => {
    */
 
   let indexControllerCallback = (request, response) => { // function declared in routes.js
-      db.tweedr.getAll((error, allTweets) => { //see db.js line 97 tweedr: tweedrModelsObject && model file tweedr_model.js line 9 getAll function - show all tweets
+      db.tweedr.getAll((error, allTweets) => {
+      // see db.js line 97 tweedr: tweedrModelsObject
+      // model file tweedr_model.js line 9
+      // getAll function - show all tweets
         console.log(allTweets);
         response.render('tweedr/index', { allTweets }); //display index html page in views folder
+      });
+  };
+
+    // ========================
+    // Show login page
+    // ========================
+    let loginControllerCallback = (request, response) => { // function declared in routes.js
+      db.tweedr.getLogin((error, allUsers) => {
+      // see db.js line 97 tweedr: tweedrModelsObject
+      // model file tweedr_model.js line 30
+      // getLogin function - show login pg
+        console.log(allUsers);
+        response.render('tweedr/login', { allUsers }); //display login html page in views folder
       });
   };
 
@@ -22,5 +38,6 @@ module.exports = (db) => {
 
   return {
     index: indexControllerCallback, // see above line 9
+    login: loginControllerCallback, // see above line 22
   };
 }
